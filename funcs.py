ca91ec055653d3ecb2b9666adde72d247ad4d777
yury-yury <yury_yury@mail.ru>
2023-02-13 23:16:44 +0300

    other updated funcs.py

--- funcs.py
+++ funcs.py
@@ -3,13 +3,15 @@ from typing import Union
 
 def sum_two_numbers(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     """
-
+    The function takes as arguments two numbers in the form of integers or real numbers and sums them up.
+    Returns the result as an integer or real type.
     """
     return a + b
 
 
 def dif_two_numbers(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     """
-
+    The function takes as arguments two numbers in the form of integers or real numbers and calculates
+    their difference. Returns the result as an integer or floating point number.
     """
     return a - b
