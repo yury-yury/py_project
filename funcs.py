7876842404c913feb78e484708c02fb471d322be
yury-yury <yury_yury@mail.ru>
2023-02-13 23:01:26 +0300

    update funcs.py

--- funcs.py
+++ funcs.py
@@ -6,3 +6,10 @@ def sum_two_numbers(a: Union[int, float], b: Union[int, float]) -> Union[int, fl
 
     """
     return a + b
+
+
+def dif_two_numbers(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """
+
+    """
+    return a - b
